5c893d5782ef32fafa838994f09d193bab701439
Shurjeel <zltm723@live.rhul.ac.uk>
2025-09-13 19:04:37 +0100

    addition and deletion of tasks complete

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ const list = document.querySelector(`.list`);
 addBtn.addEventListener(`click`, () => {
     let task = prompt(`What task do you wish to add?`)
     taskList.push(task);
-    check();
+    addItem(task);
 });
 
-function check() {
- if (taskList.length !== 0) {
-    const item = document.createElement(`li`);
-    for (const taskItem of taskList) {
+function addItem(taskItem) {
+    
+    const itemOutline = document.createElement(`div`);
+    itemOutline.setAttribute(`style`, `border-radius: 20px;
+    padding: 0.5em 1em;
+    background-color: #A8FBD3;
+    border: 2px solid rgba(0, 0, 0, 0.3);`);
+    const item = document.createElement(`p`);
+    
         item.textContent = taskItem;
-        list.appendChild(item);
-        console.log(`hey`);
-    }
-        
-}
+        itemOutline.appendChild(item);
+        list.appendChild(itemOutline);      
 }
 
+const deleteBtn = document.querySelector(`.delete`);
+deleteBtn.addEventListener(`click`, () => {
+    taskList.pop();
+    console.log(taskList + taskList.length);
+    list.removeChild(list.lastChild);
+});
